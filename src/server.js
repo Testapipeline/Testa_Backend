c4eed2c75137b56f4d4a372cac1cca12a3baec58
DanushkaNan <danushkanan@lolctech.com>
2025-03-22 00:57:55 +0530

    Fixed the refreshing issue.

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const examRoutes = require("./routes/examRoutes");
-const path = require('path');
-
 
 const app = express();
 app.use(express.json());
@@ -18,16 +16,6 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/testa/api/users", userRoutes);
 app.use("/testa/api/exams", examRoutes);
 app.use("/uploads", express.static("uploads"));
-
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    });
-}
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

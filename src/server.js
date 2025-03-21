b1f3cf69c33614372855ac211281439c9620c88f
DanushkaNan <danushkanan@lolctech.com>
2025-03-22 00:33:23 +0530

    Fixed the refreshing issue.

--- src/server.js
+++ src/server.js
@@ -17,5 +17,15 @@ app.use("/testa/api/users", userRoutes);
 app.use("/testa/api/exams", examRoutes);
 app.use("/uploads", express.static("uploads"));
 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

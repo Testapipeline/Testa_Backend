5a1e30c0b5a5236ba3c904e6f3f35fba18e213a5
DanushkaNan <danushkanan@lolctech.com>
2025-03-22 17:34:50 +0530

    Added forgot password APIs

---
+++ src/models/Otp.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const otpSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    code: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, expires: 60 }
+});
+
+module.exports = mongoose.model('Otp', otpSchema);
